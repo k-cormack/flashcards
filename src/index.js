7ac31587fb42d27fb904341ac6a24f0c46032a47
k-cormack <k.cormack@gmail.com>
2019-11-17 23:37:48 -0700

    buttons

--- src/index.js
+++ src/index.js
@@ -7,41 +7,52 @@ $(document).ready(() => {
         }
     })
 
+    let user;
     $('#start-btn').click(()=>{
-        $('#name-splash').text($('#index-textArea').val().toUpperCase());
-        $('#index-textArea').val('').hide();
-        $('#start-btn').hide();
-        if($('#name-splash').text() == 'MILA'){
-            $('#merj-pic').attr('src', './src/Images/merj/Mila-1.jpg')
-        } else if ($('#name-splash').text() == 'ELIANA'){
-            $('#merj-pic').attr('src', './src/Images/merj/Eliana-1.jpg')
-        } else if ($('#name-splash').text() == 'RILEY'){
-            $('#merj-pic').attr('src', './src/Images/merj/Riley-1.jpg')
-        } else if ($('#name-splash').text() == 'JACKSON'){
-            $('#merj-pic').attr('src', './src/Images/merj/Jackson-1.jpg')
-        } else{
-            $('#merj-pic').attr('src', './src/Images/merj/booger.jpg')
+        if($('#index-textArea').val() != ''){
+            user = $('#index-textArea').val().toUpperCase();
+            $('#name-splash').text("Hello, "+user+"!");
+            $('#start-btn').hide();
+            if(user == 'MILA'){
+                $('#merj-pic').attr('src', './src/Images/merj/Mila-1.jpg')
+            } else if (user == 'ELIANA'){
+                $('#merj-pic').attr('src', './src/Images/merj/Eliana-1.jpg')
+            } else if (user == 'RILEY'){
+                $('#merj-pic').attr('src', './src/Images/merj/Riley-1.jpg')
+            } else if (user == 'JACKSON'){
+                $('#merj-pic').attr('src', './src/Images/merj/Jackson-1.jpg')
+            } else{
+                $('#merj-pic').attr('src', './src/Images/merj/booger.jpg')
+            }
+            $('#nav-bar').hide();
+            $('#index-textArea').val('').hide();
+            $('#name-splash').show();
+            $('#img-div').show();
+            $('#begin-btn').show();
+            $('#reset-btn').show();
+        } else {
+            alert('Please enter your name or initials!')
         }
-        $('#merj-pic').show();
-        $('#begin-btn').show();
-        $('#reset-btn').show();
 
     });
 
     $('#begin-btn').click(()=>{
-        $('#nav-bar').hide();
         $('#name-splash').hide();
-        $('#merj-pic').hide();
+        $('#img-div').hide();
         $('#begin-btn').hide();
-        // $('#reset-btn').hide();
+        $('#choose-splash').text(user + ", choose the topic you want to practice below!")
+        // $('.btn-primary').text('test');
+        $('#choose-splash').show();
+        $('#choose-buttons').show();
     });
 
     $('#reset-btn').click(()=>{
         $('#name-splash').text('');
+        $('#choose-splash').hide();
         $('#nav-bar').show();
         $('#index-textArea').show();
         $('#start-btn').show();
-        $('#merj-pic').hide();
+        $('#img-div').hide();
         $('#begin-btn').hide();
         $('#reset-btn').hide();
     })
